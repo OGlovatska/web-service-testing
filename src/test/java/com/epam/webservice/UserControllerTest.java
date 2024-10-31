d8c4cc567776477e96ec78a4764e444e0d670e8a
OGlovatska <oglovatska@gmail.com>
2024-10-31 02:44:58 +0200

    Initial commit

--- src/test/java/com/epam/webservice/UserControllerTest.java
+++ src/test/java/com/epam/webservice/UserControllerTest.java
@@ -9,6 +9,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class UserControllerTest extends BaseTest {
     private final String USERS = "/users";
@@ -28,6 +29,7 @@ public class UserControllerTest extends BaseTest {
     @Test
     public void verifyResponseHeaderTest() {
         String contentType = response.getHeader("Content-Type");
+        assertTrue(response.getHeaders().hasHeaderWithName("Content-Type"), "Content-Type header is missing");
         assertEquals(contentType, "application/json; charset=utf-8", "Unexpected Content-Type");
     }
 
